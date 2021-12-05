0658bb033cc4573d97240fbf7c0eb87cc92ccd1d
Denis <den.par.al@yandex.com>
2021-12-05 03:22:42 +0300

    Added class drawer, implemented rotation and scaling for ComplexShape

--- include/core.hpp
+++ include/core.hpp
@@ -10,23 +10,31 @@ class ComplexShape
 {
 friend class Drawer;
 private:
-    std::vector<sf::Shape> shapes;
+    std::vector<std::shared_ptr<sf::Shape>> shapes;
     std::vector<sf::Vector2f> positions;
     sf::Vector2f center_position;
     float scale = 1;
+    std::size_t complexity;
 public:
-    ComplexShape(std::vector<sf::Shape> shapes, std::vector<sf::Vector2f> positions, sf::Vector2f center_position);
+    ComplexShape(std::vector<std::shared_ptr<sf::Shape>> shapes, std::vector<sf::Vector2f> positions, sf::Vector2f center_position);
     ComplexShape(const ComplexShape& shape);
     void change_size(float coef);
     sf::Color get_color(std::size_t index) const;
     void set_color(std::size_t index, sf::Color color);
     void rotate(float angle);
+    std::shared_ptr<sf::Shape> get_shape(std::size_t index) const;
+    sf::Vector2f get_coordinates(std::size_t index) const;
+    std::size_t get_complexity() const;
+    sf::Vector2f get_position() const;
 };
 
 class Drawer
 {
+protected:
+    float scale = 1;
 public:
     virtual void draw(const ComplexShape& shape) = 0;
+    void change_scale(float coef);
 };
 
 class DefaultDrawer : public Drawer
@@ -38,3 +46,47 @@ public:
     void draw(const ComplexShape& shape) override;
 };
 
+
+
+template<typename Func>
+class button
+{
+private:
+    float x;
+    float y;
+    float size_x;
+    float size_y;
+    Func action;
+    sf::Color color;
+    std::string text;
+public:
+    button(float x, float y, float size_x, float size_y, Func action, sf::Color color, std::string text): x(x), y(y), size_x(size_x), size_y(size_y), action(action), color(color), text(text) {}
+    
+    bool check_coordinate(float x, float y)
+    {
+        return x >= this->x && x <= this->x+this->size_x && y >= this->y && y <= this->y+this->size_y;
+    }
+
+    void display(sf::RenderWindow* window)
+    {
+        sf::Font font;
+        font.loadFromFile("/home/dparanic/Study/Design-Patterns-labs/Lab2.2-GeometryVisualizer/src/arial.ttf");
+
+        sf::RectangleShape rectangle(sf::Vector2f(size_x, size_y));
+        sf::Text button_text(text, font);
+        button_text.setFillColor(sf::Color::Black);
+        button_text.setOutlineThickness(1.0f);
+        button_text.setCharacterSize(25);
+        button_text.setPosition(x+20.0f,y+20.0f);
+        rectangle.setFillColor(color);
+        rectangle.setOutlineThickness(1.0f);
+        rectangle.setOutlineColor(sf::Color::Black);
+        rectangle.setPosition(x,y);
+        window->draw(rectangle);
+        window->draw(button_text);
+    }
+    void act()
+    {
+        action();
+    }
+};
--- src/core.cpp
+++ src/core.cpp
@@ -1,41 +1,69 @@
 #include "core.hpp"
+#include <iostream>
+#include <math.h>
 
 // ComplexShape
 
-    ComplexShape::ComplexShape(std::vector<sf::Shape> shapes, std::vector<sf::Vector2f> positions, sf::Vector2f center_position):shapes(shapes),
-                               positions(positions), center_position(center_position)
+    ComplexShape::ComplexShape(std::vector<std::shared_ptr<sf::Shape>> shapes, std::vector<sf::Vector2f> positions, sf::Vector2f center_position):shapes(shapes),
+                               positions(positions), center_position(center_position), complexity(shapes.size())
     {
         for(auto& shape : shapes)
         {
-            shape.setOrigin(center_position);
+            shape->setOrigin(shape->getLocalBounds().width/2.0f, shape->getLocalBounds().height/2.0f);
         }
     }
 
-    ComplexShape::ComplexShape(const ComplexShape& shape): shapes(shape.shapes), positions(shape.positions), center_position(shape.center_position), scale(shape.scale) {}
+    ComplexShape::ComplexShape(const ComplexShape& shape): shapes(shape.shapes), positions(shape.positions), center_position(shape.center_position),
+                               scale(shape.scale), complexity(shape.complexity) {}
 
     void ComplexShape::change_size(float coef)
     {
         scale = coef;
         for(auto& shape : shapes)
         {
-            shape.setScale(coef,coef);
+            shape->setScale(coef,coef);
         }
     }
     sf::Color ComplexShape::get_color(std::size_t index) const
     {
-        return shapes[index].getFillColor();
+        return shapes[index]->getFillColor();
     }
     void ComplexShape::set_color(std::size_t index, sf::Color color)
     {
-        shapes[index].setFillColor(color);
-        shapes[index].setOutlineColor(color);
+        shapes[index]->setFillColor(color);
+        shapes[index]->setOutlineColor(color);
     }
     void ComplexShape::rotate(float angle)
     {
-        for(auto& shape : shapes)
+        float rad = (angle*3.14f)/180.0f;
+        float cs = std::cos(rad);
+        float sn = std::sin(rad);
+        for(std::size_t i = 0; i < complexity; i++)
         {
-            shape.rotate(angle);
+            shapes[i]->rotate(angle);
+            sf::Vector2f vec = positions[i];
+            positions[i]=sf::Vector2f(vec.x*cs - vec.y*sn, vec.x*sn + vec.y*cs);
         }
+    }
+
+    std::shared_ptr<sf::Shape> ComplexShape::get_shape(std::size_t index) const
+    {
+        return shapes[index];
+    }
+
+    sf::Vector2f ComplexShape::get_coordinates(std::size_t index) const
+    {
+        return positions[index];
+    }
+
+    std::size_t ComplexShape::get_complexity() const
+    {
+        return complexity;
+    }
+
+    sf::Vector2f ComplexShape::get_position() const
+    {
+        return center_position;
     }
 
 //~ComplexShape
@@ -44,8 +72,23 @@
 
 DefaultDrawer::DefaultDrawer(std::shared_ptr<sf::RenderTexture> picture) : picture(picture) {}
 
+void Drawer::change_scale(float coef)
+{
+    scale = coef;
+}
+
 void DefaultDrawer::draw(const ComplexShape& shape)
 {
-    
+    for(std::size_t i = 0; i < shape.get_complexity(); ++i)
+    {
+        std::shared_ptr<sf::Shape> tmp_shape = shape.get_shape(i);
+        sf::Vector2f central_p = shape.get_position();
+        sf::Vector2f shape_p = shape.get_coordinates(i);
+        central_p.x += shape_p.x * scale;
+        central_p.y += shape_p.y * scale;
+        tmp_shape->setPosition(central_p);
+        tmp_shape->scale(scale, scale);
+        picture->draw(*tmp_shape);
+    }
 }
 //~DefaultDrawer
